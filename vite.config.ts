40dbadba66bcedca0251b5a05b68a8e61a029ca0
Tim Engel <tim.engel.lp@gmail.com>
2026-04-03 20:24:38 +0200

    ops: fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/present/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/present/' : '/',
   plugins: [vue()],
-})
+}))
